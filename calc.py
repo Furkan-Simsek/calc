ba736a5415ad18d8a309aab11aac1d22c368cb11
Furkan Şimşek <83591761+Furkan-Simsek@users.noreply.github.com>
2021-05-04 12:51:30 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,13 @@
+number1 = int(input("Birinci Sayı: "))
+number2 = int(input("İkinci Sayı: "))
+a = input("Çarpma ise '*' bas, Bölme ise '/' bas, Toplama ise'+'bas, Çıkarma ise '-' bas. ")
+
+if a == "*":
+    print(number1 * number2)
+if a == "/":
+    print(number1 / number2)
+if a == "+":
+    print(number1 + number2)
+if a == "-":
+    print(number1 - number2)
+
